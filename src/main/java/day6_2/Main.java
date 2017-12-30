ce153f3e82b2a8e9e0a3f90b4c7c9145c5c1d846
Anders Persson <anders.erik.persson@gmail.com>
2017-12-30 12:25:36 +0100

    6 - 2

---
+++ src/main/java/day6_2/Main.java
@@ -0,0 +1,54 @@
+package day6_2;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Main main = new Main();
+        int[] banks = {14, 0, 15, 12, 11, 11, 3, 5, 1, 6, 8, 4, 9, 1, 8, 4};
+        //int[] banks = {0,2,7,0};
+        System.out.println("Result=" + main.solve(banks));
+    }
+
+    private long solve(int[] banks) {
+        Map<String, Integer> oldBanks = new Hashtable<>();
+
+        int cycles = 0;
+
+        while (!oldBanks.containsKey(banksToString(banks))) {
+            cycles++;
+            oldBanks.put(banksToString(banks), cycles);
+            redistribute(banks);
+        }
+        return cycles - oldBanks.get(banksToString(banks)) + 1;
+    }
+
+    private String banksToString(int[] banks) {
+        StringBuilder builder = new StringBuilder(banks.length);
+        Arrays.stream(banks).forEach(builder::append);
+        return builder.toString();
+    }
+
+    private void redistribute(int[] banks) {
+        int redisValue = 0;
+        int redisIndex = 0;
+        for (int index = 0; index < banks.length; index++) {
+            if (banks[index] > redisValue) {
+                redisValue = banks[index];
+                redisIndex = index;
+            }
+        }
+        banks[redisIndex] = 0;
+        while (redisValue > 0) {
+            redisIndex = (redisIndex + 1) % banks.length;
+            banks[redisIndex]++;
+            redisValue--;
+        }
+    }
+}
