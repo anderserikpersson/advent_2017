2e8b4ca7ddb83be3fd0e72b620cc0fa3caa426f3
Anders Persson <anders.erik.persson@gmail.com>
2018-12-02 12:50:35 +0100

    Day 2

--- src/main/java/aoc2018_2/a2/Main.java
+++ src/main/java/aoc2018_2/a2/Main.java
@@ -1,12 +1,14 @@
 package aoc2018_2.a2;
 
+import com.google.common.collect.Lists;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -16,47 +18,83 @@ public class Main {
 
         List<String> ids = Files.lines(Paths.get(input)).sorted().collect(Collectors.toList());
 
-        System.out.println(solve(ids));
-
+        List<DiffResult> result = solve(ids);
+        if (result.size() == 1) {
+            String s = result.get(0).getStr1();
+            int pos = result.get(0).getDiffPositions().get(0);
+            System.out.println("Svar:" + s.substring(0, pos) + s.substring(pos + 1));
+        } else {
+            System.out.println("Konstigt resultat?!");
+        }
     }
 
-    private static Optional<String> solve(List<String> ids) {
+    private static List<DiffResult> solve(List<String> ids) {
         int idNr = 0;
 
         while (idNr < ids.size()) {
             String idStr = ids.get(idNr);
-            List<String> match = ids.subList(idNr, ids.size()).stream().filter(otherStr -> diffOneChar(idStr, otherStr)).collect(Collectors.toList());
+            List<DiffResult> match = ids.subList(idNr, ids.size())
+                    .stream()
+                    .map(otherStr -> diff(idStr, otherStr))
+                    .filter(diffResult -> diffResult.getDiffPositions().size() == 1)
+                    .collect(Collectors.toList());
             if (match.size() > 0) {
-                match.forEach(System.out::println);
-                return Optional.of(idStr);
+                return match;
             }
             idNr++;
         }
-        return Optional.empty();
+        return Collections.emptyList();
     }
 
-    private static boolean diffOneChar(String str1, String str2) {
-        int diffCounter = 0;
+    private static DiffResult diff(String str1, String str2) {
+        DiffResult diffResult = new DiffResult(str1, str2);
         int pos = 0;
-        while (pos < str1.length() && diffCounter <= 1) {
+
+        while (pos < str1.length()) {
             if (str1.charAt(pos) != str2.charAt(pos)) {
-                diffCounter++;
+                diffResult.addPosition(pos);
             }
             pos++;
         }
-        return diffCounter == 1;
+        return diffResult;
     }
 
-    private class DiffResult {
+    private static class DiffResult {
         String str1;
         String str2;
-        int pos;
+        List<Integer> diffPositions = Lists.newArrayList();
 
-        public DiffResult(String str1, String str2, int pos) {
+        private DiffResult(String str1, String str2) {
             this.str1 = str1;
             this.str2 = str2;
-            this.pos = pos;
+        }
+
+        public String getStr1() {
+            return str1;
+        }
+
+        public void setStr1(String str1) {
+            this.str1 = str1;
+        }
+
+        public String getStr2() {
+            return str2;
+        }
+
+        public void setStr2(String str2) {
+            this.str2 = str2;
+        }
+
+        public List<Integer> getDiffPositions() {
+            return diffPositions;
+        }
+
+        public void setDiffPositions(List<Integer> diffPositions) {
+            this.diffPositions = diffPositions;
+        }
+
+        public void addPosition(Integer pos) {
+            this.diffPositions.add(pos);
         }
     }
-
 }
